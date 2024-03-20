df096aba4dd556eb2e1b88920f0c73e289942701
Batty-sk <saorav.skumar@gmail.com>
2024-03-21 00:00:43 +0530

    signup page completion

---
+++ src/const.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const loginOrRegister = async (data, url) => {
+    try {
+        const res = await axios.post(`http://localhost:8000/${url}`, {
+            username: data.username,
+            password: data.password
+        });
+        if (res.status !== 200) {
+            throw new Error(res.error);
+        }
+        localStorage.setItem('username', res.data.username);
+        localStorage.setItem('password', res.data.password);
+    } catch (error) {
+        throw error; 
+    }
+};
+
+try {
+    await loginOrRegister(data, url);
+} catch (err) {
+    console.log(err);
+}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,28 +1,40 @@
+import { loginOrRegister } from '../const';
+
 import React from 'react';
+
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { BsPersonFill, BsLockFill } from 'react-icons/bs'; // Importing icons from React Icons
+import { BsPersonFill, BsLockFill } from 'react-icons/bs';
+
 
 const Signup = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const onSubmission = (data) => {
-    console.log(data);
+  const onSubmission = async (data) => {
+    console.log( 'data ', data)
+    try{
+        await loginOrRegister(data,'register')
+
+    }
+    catch(error){
+        console.log(error)
+    }   
+
   };
 
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+    <Container fluid className="d-flex align-items-center justify-content-center" style={{ height: '100vh',background:'#169ba3' }}>
       <Row className="justify-content-center  ">
         <Col xs={12} md={12}>
-          <Form onSubmit={handleSubmit(onSubmission)} style={{border:'1px solid black',padding:'20px'}} >
+          <Form onSubmit={handleSubmit(onSubmission)} style={{border:'1px solid black',padding:'40px',background:'#2b355b',borderRadius:'20px',boxShadow:'0px 10px 20px black'}} >
 
-            <h2 className='text-center'>Sign Up</h2>
+            <h2 className='text-center p-3 rounded mb-3' style={{background:'cyan'}}>Sign Up</h2>
 
             <div className="text-center mb-3">
-              <BsPersonFill size={64} className="border border-dark rounded-circle p-2" />
+              <BsPersonFill size={64} style={{color:'white'}} className="border rounded-circle p-2" />
             </div>
-            <Form.Group controlId="formBasicEmail">
-              <div className="input-group">
+            <Form.Group controlId="formBasicEmail" className='mb-3'>
+              <div className="input-group d-flex align-items-center">
                 
                 <div className="input-group-prepend">
                   <span className="input-group-text"><BsPersonFill /></span>
@@ -33,8 +45,8 @@ const Signup = () => {
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <div className="input-group">
-                <div className="input-group-prepend">
+              <div className="input-group d-flex align-items-center">
+                 <div className="input-group-prepend">
                   <span className="input-group-text"><BsLockFill /></span> 
                 </div>
                 <Form.Control type="password" placeholder="Password" {...register("password",{required:true})} />
@@ -42,14 +54,15 @@ const Signup = () => {
               {errors.password && <span className='text-danger'>Password is required</span>}
             </Form.Group>
 
-            <div className='mt-4'>
-            <Button variant="primary" type="submit">
-              Submit
+            <div className='mt-4 d-flex justify-content-center'>
+            <Button style={{background:'cyan',color:'black'}} type="submit">
+              Signup
             </Button>
-            <Form.Text className="text-muted">
-              <a href="login/">Already have an account? Sign In</a>
-            </Form.Text>
+           
             </div>
+            <Form.Text className="text-muted">
+              <a href="login/" className='text-white'>Already have an account? Sign In</a>
+            </Form.Text>
           </Form>
         </Col>
       </Row>
